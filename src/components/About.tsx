848479e65ebe80acd6e88b32097f5078f607419b
Balakannan <balakannan2522@gmail.com>
2025-06-06 16:01:04 +0530

    update: responsive design

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,11 +14,13 @@ export default function About() {
     - Migrated charts to ECharts for performance boost
     - Developed low-code/no-code BPMN workflows with Form.io
     - Built reusable Node.js components (70% less redundant code)
+    - Optimized data Structure by separating data entry logic (30% faster response time)
   skills:
-    languages: ["C#", "C++", "JavaScript", "TypeScript", "Python"]
+    languages: ["C#", "C++", "JavaScript", "TypeScript", "Python", "Go"]
     databases: ["SQL", "MongoDB", "Redis"]
-    frameworks: [".NET", "Node.js", "Express.js", "Angular", "React"]
-    tools: ["Git", "Docker", "Azure", "Bootstrap", "Material UI"]
+    frameworks: [".NET", "Node.js", "Express.js", "Angular", "React", "React Native"]
+    tools: ["Git", "Docker", "Azure", "Kubernetes", "CI/CD", "Strapi", "Bootstrap", "Material UI"]
+    Testing: ["Jest", "Selenium", "Postman"]
   interests:
     - "Distributed Systems"
     - "Microservices Architecture"
@@ -32,7 +34,8 @@ export default function About() {
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-        padding: "2em 0",
+        px: { xs: ".5em", sm: "2em" },
+        py: "2em",
       }}
     >
       <Typography
@@ -45,8 +48,8 @@ export default function About() {
           borderTopRightRadius: "8px",
           fontFamily: "monospace",
           fontSize: "14px",
-          width: "90%",
-          maxWidth: "50vw",
+          width: "100%", //{ xs: "100%", md: "90%" },
+          maxWidth: { xs: "90vw", md: "70vw", lg: "60vw" },
         }}
       >
         about.yaml
@@ -58,8 +61,8 @@ export default function About() {
           borderBottomLeftRadius: "8px",
           borderBottomRightRadius: "8px",
           paddingInline: "1em",
-          width: "90%",
-          maxWidth: "50vw",
+          width: "100%", //{ xs: "100%", md: "90%" },
+          maxWidth: { xs: "90vw", md: "70vw", lg: "60vw" },
           boxShadow: "0px 4px 12px 5px rgba(0,0,0,0.3)",
           textAlign: "left",
         }}
